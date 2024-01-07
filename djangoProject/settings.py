f1c3f970b4cdb250f8d62c40153622cb07234634
Asel Hemantha <e19300@eng.pdn.ac.lk>
2024-01-07 22:02:40 +0530

    Update settings.py for development

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
