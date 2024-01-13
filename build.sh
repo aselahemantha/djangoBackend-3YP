19603f847b51a65f6e21d0d6a00203cdfcbe4a45
Asel Hemantha <e19300@eng.pdn.ac.lk>
2024-01-14 00:42:39 +0530

    Update project for development

--- build.sh
+++ build.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-# Assuming the script is in the root directory
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pip install -r requirements.txt
 
-# Activate virtual environment
-source "${SCRIPT_DIR}/venv/bin/activate"
-
-# Install requirements
-pip install -r "${SCRIPT_DIR}/requirements.txt"
-
-# Run migrations
-python "${SCRIPT_DIR}/manage.py" migrate
-
-# Collect static files
-python "${SCRIPT_DIR}/manage.py" collectstatic --no-input
+python manage.py collectstatic --no-input
+python manage.py migrate
