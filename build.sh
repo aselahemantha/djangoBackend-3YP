68aa70913a114aa7a8a0b4fdcbb63e89b153ac08
Asel Hemantha <e19300@eng.pdn.ac.lk>
2024-01-18 18:12:17 +0530

    add pip3

--- build.sh
+++ build.sh
@@ -2,20 +2,9 @@
 # exit on error
 set -o errexit
 
-echo "Creating virtual environment..."
 python3 -m venv .venv
-echo "Virtual environment created."
-
-echo "Activating virtual environment..."
 . .venv/bin/activate
-echo "Virtual environment activated."
+pip3 install -r requirements.txt
 
-echo "Collecting static files..."
 python manage.py collectstatic --no-input
-echo "Static files collected."
-
-echo "Applying database migrations..."
 python manage.py migrate
-echo "Database migrations applied."
-
-echo "Build script completed successfully."
