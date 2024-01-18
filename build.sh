d1388d16ecb0647956f1f79dc39d7975b10f2122
Asel Hemantha <e19300@eng.pdn.ac.lk>
2024-01-18 17:57:04 +0530

    Upload venv also for render development

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+python3 -m venv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
