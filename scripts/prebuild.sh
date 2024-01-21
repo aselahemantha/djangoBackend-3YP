a8d0ab523df43c89bfa57afe62df38dbdb5f47af
Asel Hemantha <e19300@eng.pdn.ac.lk>
2024-01-21 10:02:17 +0530

    Create prebuild.sh to run when the development happens

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Update package lists
-apt-get update
+sudo apt-get update
 
 # Install required packages
-apt-get install -y build-essential cmake
-apt-get install -y libopenblas-dev liblapack-dev
-apt-get install -y libx11-dev libgtk-3-dev
-apt-get install -y libgl1-mesa-glx
-apt-get install -y libglib2.0-dev
+sudo apt-get install -y build-essential cmake
+sudo apt-get install -y libopenblas-dev liblapack-dev
+sudo apt-get install -y libx11-dev libgtk-3-dev
+sudo apt-get install -y libgl1-mesa-glx
+sudo apt-get install -y libglib2.0-dev
