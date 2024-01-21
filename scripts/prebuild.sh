6708a0406c03a09d7cbdf8395c6b0dbce9297e42
Asel Hemantha <e19300@eng.pdn.ac.lk>
2024-01-21 11:03:47 +0530

    Update requirements.txt for prduction

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -7,3 +7,5 @@ apt-get update
 apt-get install -y build-essential cmake
 apt-get install -y libopenblas-dev liblapack-dev
 apt-get install -y libx11-dev libgtk-3-dev
+apt-get install -y libgl1-mesa-glx
+apt-get install -y libglib2.0-dev
